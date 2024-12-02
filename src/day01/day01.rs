ea1d2461e9bec402d96b061b80095d7f94305707
aem <alain.e.matthews@protonmail.com>
2024-12-01 18:05:17 -0700

    minor cleanup w/o changing the orig solution

--- src/day01/day01.rs
+++ src/day01/day01.rs
@@ -1,34 +1,22 @@
 use std::{collections::HashMap, vec::Vec};
-pub fn part_one((input1, input2): &(Vec<i32>, Vec<i32>)) -> i32 {
-    let len1 = input1.len();
-    let len2 = input2.len();
-    if len1 != len2 {
-        return 0;
-    }
-    let mut left = input1.clone();
-    let mut right = input2.clone();
+
+type Input = (Vec<i32>, Vec<i32>);
+
+pub fn part_one(input: &Input) -> i32 {
+   
+    let (mut left, mut right) = input.clone();
     left.sort();
     right.sort();
 
-    let mut cache1: HashMap<usize, i32> = HashMap::new();
-    let mut cache2: HashMap<usize, i32> = HashMap::new();
-
-    for (index, value) in left.into_iter().enumerate() {
-        cache1.insert(index, value);
-    }
-    for (index, value) in right.into_iter().enumerate() {
-        cache2.insert(index, value);
-    }
     let mut sum = 0;
-    for i in 0..len1 {
-        let num: i32 = *(cache1.get(&i).unwrap()) - *(cache2.get(&i).unwrap());
-        sum += num.abs()
+    for i in 0..left.len(){
+        sum += (left[i]-right[i]).abs();
     }
-
     return sum;
 }
 
-pub fn part_two((left, right): &(Vec<i32>, Vec<i32>)) -> i32 {
+pub fn part_two(input: &Input) -> i32 {
+    let (left, right) = input;
     let mut freq_left: HashMap<i32, i32> = HashMap::with_capacity(1000);
     let mut freq_right: HashMap<i32, i32> = HashMap::with_capacity(1000);
     for elem in left.iter() {
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-use aoc::utils::parse;
 use aoc::day01::day01;
+use aoc::utils::parse;
 
 fn main() {
-    
-    let day01_p1_path = "input/day01.txt";
-    let input_res = parse::parse_columns_from_file(&day01_p1_path);
-    let input = match input_res{
+    let day01_path = "input/day01.txt";
+    let input = match parse::parse_columns_from_file(&day01_path) {
         Ok(res) => res,
         Err(error) => panic!("Problem opening the file: {error:?}"),
-        };
+    };
 
     println!("Part One: {}", day01::part_one(&input));
     println!("Part Two: {}", day01::part_two(&input));
-
 }
-
